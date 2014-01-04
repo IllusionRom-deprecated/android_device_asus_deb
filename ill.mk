0aac1be15d37f607a844dac7d7731278281dfe05
Hieu Phan <aznrice2k4@gmail.com>
2014-01-04 08:34:19 -0600

    Update Illusion
    Change-Id: I3c3e0a9ead4a53d9d007bbcd6069d7b5e8f04f33

--- ill.mk
+++ ill.mk
@@ -1,3 +1,4 @@
+$(call inherit-product, vendor/illusion/config/common.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
@@ -7,18 +8,16 @@ PRODUCT_RELEASE_NAME := deb
 
 $(call inherit-product, device/asus/deb/full_deb.mk)
 
-$(call inherit-product, vendor/illusion/config/common.mk)
-
 $(call inherit-product, vendor/illusion/config/gsm.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := deb
-PRODUCT_NAME := illusion_deb
+PRODUCT_NAME := ill_deb
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := asus
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=razorg BUILD_FINGERPRINT="google/razorg/deb:4.4/KRT16O/907817:user/release-keys" PRIVATE_BUILD_DESC="razorg-user 4.4 KRT16O 907817 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=razorg BUILD_FINGERPRINT="google/razorg/deb:4.4.2/KOT49H/937116:user/release-keys" PRIVATE_BUILD_DESC="razorg-user 4.4.2 KOT49H 937116 release-keys"
 
 # bootanimation
 PRODUCT_COPY_FILES += \
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -15,4 +15,4 @@
 #
 
 add_lunch_combo aosp_deb-userdebug
-add_lunch_combo illusion_deb-userdebug
+add_lunch_combo ill_deb-userdebug
